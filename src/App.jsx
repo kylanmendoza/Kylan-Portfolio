070df375cee0cde3e1af3c0fde7982357cd8c12c
Kylan Mendoza <kylanmendoza@gmail.com>
2025-12-16 21:21:16 -0800

    Currently beginning project setup. Installed npm dependencies, tailwind css, vite-react, emailjs-com

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import NavBar from './components/NavBar.jsx'
+
+function App() {
+  // const [count, setCount] = useState(5)
+
+  return (
+    <>
+      <div>
+        <NavBar></NavBar>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+export default function NavBar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="bg-gray-900">
+        <h1 className="text-5xl font-extrabold bg-clip-text bg-gradient-to-r from-indigo-400">Kylan Mendoza</h1>
+    </header>
+  )
+}
