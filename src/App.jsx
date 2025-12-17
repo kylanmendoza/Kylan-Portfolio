e10b24ffa6ba9ea5d1256379cde933895e822162
Kylan Mendoza <kylanmendoza@gmail.com>
2025-12-16 23:08:43 -0800

    Created loading screen component along with typing animation, and loading bar. Also began building NavBar component with  added 'ky.tech' at the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
-import NavBar from './components/NavBar.jsx'
+import { NavBar } from './components/NavBar.jsx';
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(5)
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-      <div>
-        <NavBar></NavBar>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+      <div className={ `min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100` }>
+        <NavBar />
+
+
       </div>
     </>
-  )
+  );
 }
+
+
+
+
+
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,64 @@
-'use client'
+// 'use client'
 
-import { useState } from 'react'
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from '@headlessui/react'
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+// import { useState } from 'react'
+// import {
+//   Dialog,
+//   DialogPanel,
+//   Disclosure,
+//   DisclosureButton,
+//   DisclosurePanel,
+//   Popover,
+//   PopoverButton,
+//   PopoverGroup,
+//   PopoverPanel,
+// } from '@headlessui/react'
+// import {
+//   ArrowPathIcon,
+//   Bars3Icon,
+//   ChartPieIcon,
+//   CursorArrowRaysIcon,
+//   FingerPrintIcon,
+//   SquaresPlusIcon,
+//   XMarkIcon,
+// } from '@heroicons/react/24/outline'
+// import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
-const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
+// const products = [
+//   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+//   { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+//   { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+//   { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+//   { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+// ]
+// const callsToAction = [
+//   { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+//   { name: 'Contact sales', href: '#', icon: PhoneIcon },
+// ]
 
-export default function NavBar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  return (
-    <header className="bg-gray-900">
-        <h1 className="text-5xl font-extrabold bg-clip-text bg-gradient-to-r from-indigo-400">Kylan Mendoza</h1>
-    </header>
-  )
-}
+// export default function NavBar() {
+//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+//   return (
+//     <header className="bg-gray-900">
+//         <h1 className="text-5xl font-extrabold bg-clip-text bg-gradient-to-r from-indigo-400">Kylan Mendoza</h1>
+//     </header>
+//   )
+// }
+
+
+
+export const NavBar = () => {
+    return (
+        <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-white/10 shadow-lg">
+            <div className="max-w-5xl mx-auto px-4">
+                <div className="flex justify-between items-center h-16">
+                    <a href="#home" className="font-mono text-xl font-bold text-white">
+                        {" "}
+                        ky<span className="text-blue-500">.tech</span>{" "}
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
