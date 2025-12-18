3f803a9a678c04bfa5d9cb38b3c9c36cf18950be
Kylan Mendoza <kylanmendoza@gmail.com>
2025-12-17 17:52:30 -0800

    Created navigation to other section pages - Created hamburger icon for mobile viewing as well as list of section nav links upon interacting - Started creating 'Home' page component with intro styling applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
 import { NavBar } from './components/NavBar.jsx';
 import { LoadingScreen } from "./components/LoadingScreen";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from './components/sections/home.jsx';
 import "./index.css";
 import "./App.css";
+
 
 function App() {
   // const [count, setCount] = useState(5)
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
-      <div className={ `min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100` }>
-        <NavBar />
-
-
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div 
+        className={ `min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+        >
+          <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Home />
       </div>
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 // 'use client'
 
-// import { useState } from 'react'
+
 // import {
 //   Dialog,
 //   DialogPanel,
@@ -46,9 +46,16 @@
 //   )
 // }
 
+import { useEffect } from "react";
 
 
-export const NavBar = () => {
+export const NavBar = ({ menuOpen, setMenuOpen }) => {
+
+
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
@@ -57,6 +64,45 @@ export const NavBar = () => {
                         {" "}
                         ky<span className="text-blue-500">.tech</span>{" "}
                     </a>
+
+                    <div 
+                        className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
+                        onClick={() => setMenuOpen((prev) => !prev)}
+                    >
+                        &#9776;
+                    </div>
+
+                    <div className="hidden md:flex items-center space-x-8">
+                    <a 
+                        href="#home" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                        >
+                            {" "}
+                            Home {" "}
+                        </a>
+                        <a 
+                        href="#about" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                        >
+                            {" "}
+                            About {" "}
+                        </a>
+                        <a 
+                        href="#projects" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                        >
+                            {" "}
+                            Projects {" "}
+                        </a>
+                        <a 
+                        href="#contact" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                        >
+                            {" "}
+                            Contact {" "}
+                        </a>
+
+                    </div>
                 </div>
             </div>
         </nav>
